66345307e184e3133b596b01a41a00b9c6cf5141
Tumer124 <egor.bugaev2015@yandex.ru>
2023-11-01 19:57:19 +0700

    exam

---
+++ main.py
@@ -0,0 +1,10 @@
+def chislo(num):
+    if num/10>1:
+        num = chislo(sum([int(i) for i in str(num)]))
+    else:
+        return num
+    if num/10<1:
+        return num
+
+print(chislo(38))
+print(chislo(12))
